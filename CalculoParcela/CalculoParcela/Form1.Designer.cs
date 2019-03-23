ad90fbc5ad8133fa974055a8364f36a92870e500
Jonas Valereo <jvalereo2016@gmail.com>
2019-03-23 01:01:11 -0300

    Programa básico Calculo de Parcela
    Projeto, programa básico em C#, calculo de parcela

---
+++ CalculoParcela/CalculoParcela/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CalculoParcela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblTitulo2 = new System.Windows.Forms.Label();
+            this.lblValorTotal = new System.Windows.Forms.Label();
+            this.lblParcela = new System.Windows.Forms.Label();
+            this.textValorTotal = new System.Windows.Forms.TextBox();
+            this.textParcela = new System.Windows.Forms.TextBox();
+            this.BtnCalcular = new System.Windows.Forms.Button();
+            this.BtnLimpa = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.textResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(72, 17);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(149, 22);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "CALCULE SUA ";
+            // 
+            // lblTitulo2
+            // 
+            this.lblTitulo2.AutoSize = true;
+            this.lblTitulo2.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo2.Location = new System.Drawing.Point(105, 39);
+            this.lblTitulo2.Name = "lblTitulo2";
+            this.lblTitulo2.Size = new System.Drawing.Size(84, 23);
+            this.lblTitulo2.TabIndex = 1;
+            this.lblTitulo2.Text = "PARCELA";
+            // 
+            // lblValorTotal
+            // 
+            this.lblValorTotal.AutoSize = true;
+            this.lblValorTotal.BackColor = System.Drawing.Color.Transparent;
+            this.lblValorTotal.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorTotal.Location = new System.Drawing.Point(27, 114);
+            this.lblValorTotal.Name = "lblValorTotal";
+            this.lblValorTotal.Size = new System.Drawing.Size(84, 18);
+            this.lblValorTotal.TabIndex = 2;
+            this.lblValorTotal.Text = "Valor Total:";
+            // 
+            // lblParcela
+            // 
+            this.lblParcela.AutoSize = true;
+            this.lblParcela.BackColor = System.Drawing.Color.Transparent;
+            this.lblParcela.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParcela.Location = new System.Drawing.Point(27, 164);
+            this.lblParcela.Name = "lblParcela";
+            this.lblParcela.Size = new System.Drawing.Size(66, 18);
+            this.lblParcela.TabIndex = 3;
+            this.lblParcela.Text = "Parcela:";
+            // 
+            // textValorTotal
+            // 
+            this.textValorTotal.Location = new System.Drawing.Point(109, 114);
+            this.textValorTotal.Name = "textValorTotal";
+            this.textValorTotal.Size = new System.Drawing.Size(137, 20);
+            this.textValorTotal.TabIndex = 4;
+            // 
+            // textParcela
+            // 
+            this.textParcela.Location = new System.Drawing.Point(109, 165);
+            this.textParcela.Name = "textParcela";
+            this.textParcela.Size = new System.Drawing.Size(137, 20);
+            this.textParcela.TabIndex = 5;
+            // 
+            // BtnCalcular
+            // 
+            this.BtnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalcular.Location = new System.Drawing.Point(60, 253);
+            this.BtnCalcular.Name = "BtnCalcular";
+            this.BtnCalcular.Size = new System.Drawing.Size(83, 35);
+            this.BtnCalcular.TabIndex = 6;
+            this.BtnCalcular.Text = "Calcular";
+            this.BtnCalcular.UseVisualStyleBackColor = true;
+            this.BtnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // BtnLimpa
+            // 
+            this.BtnLimpa.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpa.Location = new System.Drawing.Point(158, 253);
+            this.BtnLimpa.Name = "BtnLimpa";
+            this.BtnLimpa.Size = new System.Drawing.Size(78, 35);
+            this.BtnLimpa.TabIndex = 7;
+            this.BtnLimpa.Text = "Limpar";
+            this.BtnLimpa.UseVisualStyleBackColor = true;
+            this.BtnLimpa.Click += new System.EventHandler(this.BtnLimpa_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(111, 306);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(80, 33);
+            this.BtnSair.TabIndex = 8;
+            this.BtnSair.Text = "Fechar";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.Transparent;
+            this.lblResultado.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(27, 213);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(82, 18);
+            this.lblResultado.TabIndex = 9;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // textResultado
+            // 
+            this.textResultado.Location = new System.Drawing.Point(111, 213);
+            this.textResultado.Name = "textResultado";
+            this.textResultado.Size = new System.Drawing.Size(135, 20);
+            this.textResultado.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(298, 360);
+            this.ControlBox = false;
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnLimpa);
+            this.Controls.Add(this.BtnCalcular);
+            this.Controls.Add(this.textParcela);
+            this.Controls.Add(this.textValorTotal);
+            this.Controls.Add(this.lblParcela);
+            this.Controls.Add(this.lblValorTotal);
+            this.Controls.Add(this.lblTitulo2);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de Parcela";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblTitulo2;
+        private System.Windows.Forms.Label lblValorTotal;
+        private System.Windows.Forms.Label lblParcela;
+        private System.Windows.Forms.TextBox textValorTotal;
+        private System.Windows.Forms.TextBox textParcela;
+        private System.Windows.Forms.Button BtnCalcular;
+        private System.Windows.Forms.Button BtnLimpa;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox textResultado;
+    }
+}
+
---
+++ CalculoParcela/CalculoParcela/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoParcela
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            // Aula Calcular Parcela
+            
+            //Declarando as variaveis
+
+            double valorTotal, parcela, resultado;
+            valorTotal = Convert.ToDouble(textValorTotal.Text);
+            parcela = Convert.ToDouble(textParcela.Text);
+            resultado = (valorTotal / parcela);
+
+            //Declarando o buttom calcular
+
+            textResultado.Text = resultado.ToString();
+
+            MessageBox.Show("Valor da Parcela é!!!","Messagem");
+
+            //fim do programa
+
+        }
+
+        private void BtnLimpa_Click(object sender, EventArgs e)
+        {
+            //Declarando codigo limpar das textbox
+
+            textValorTotal.Text = " ";
+            textParcela.Text = " ";
+            textResultado.Text = " ";
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            //Declarando codigo fechar
+
+            Close();
+
+        }
+    }
+}
